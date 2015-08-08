74ad1777899c89ecb896244cac60410ffb616573
Alex Yiznitsy <yiznitsky@gmail.com>
2015-08-08 15:21:58 -0700

    Change pay rate

--- paycheck.py
+++ paycheck.py
@@ -2,13 +2,13 @@
 
 print "How many weekday hours?", 51
 
-print "weekday hour rate?", 25
+print "weekday hour rate?", 45
 
 print "How many weekend hours", 2.5
 
 print "weekday hour rate?", 28
 
-print "total weekday pay", 51 * 25
+print "total weekday pay", 51 * 45
 print "total weekend pay", 2.5 * 28
 
 print" total", (51 * 25) + ( 2.5 * 28)
